dfa0bc365f1d0086bb8af9d823174ead28d6250f
Mark Yang <haker0x04@163.com>
2024-05-25 21:28:31 +0800

    完成第一版head 的css设计

---
+++ src/views/layout/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <link rel="stylesheet" href="src/assets/css/layout.css">
+
+    <!-- 头 -->
+    <div class="head">
+        <div class="logo"><img src="/src/assets//icons/logo.png" alt=""></div>
+        <div class="buttons">
+            <div class="bt1">登录</div>
+            <div class="bt2">注册</div>
+        </div>
+    </div>
+
+    <!-- cover -->
+    <!-- under -->
+</template>
+
+<script lang="ts" setup>
+</script>
