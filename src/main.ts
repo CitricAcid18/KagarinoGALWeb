14eb770b3f5c0887d0f58bf29928d307658e6e58
Mark Yang <haker0x04@163.com>
2024-05-26 15:26:21 +0800

    完成under区域

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import './assets/css/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -11,8 +11,84 @@
     </div>
 
     <!-- cover -->
+    <div class="cover">
+        <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+            <li v-for="i in count" :key="i" class="infinite-list-item">{{ i }}</li>
+        </ul>
+    </div>
     <!-- under -->
+
+    <div class="under">
+        <div class="box">
+            <div class="information">
+                <h2>站点信息</h2>
+                <ul>
+                    <li><a href="#">意见反馈</a></li>
+                    <li><a href="#">免责声明</a></li>
+                    <li><a href="#">关于我们</a></li>
+                </ul>
+            </div>
+            <div class="friends">
+                <h2>友情链接</h2>
+                <ul>
+                    <li><a href="#">xx</a></li>
+                    <li><a href="#">xx</a></li>
+                    <li><a href="#">xxx</a></li>
+                </ul>
+            </div>
+            <div class="uwants">
+                <h2>讨论区</h2>
+                <ul>
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">参与贡献</a></li>
+                </ul>
+            </div>
+        </div>
+        <p>Powered by <a href="#"><span>KAIO Team</span><br></a> <a href="#"><span>ICP备案 12345678</span></a><a href="#"><span>公网安备 12345678</span></a></p>
+    </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+import xx from 'element-plus';
+const count = ref(0)
+const load = () => {
+    if (count.value > 10){
+        return
+    }
+    count.value += 2
+    
+}
+
 </script>
+
+<style>
+.cover {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.cover .infinite-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 1px 1px;
+    height: 100vh;
+    width: 90vw;
+}
+
+.cover .infinite-list .infinite-list-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 240px;
+    width: 290px;
+    background: var(--root-white);
+    margin: 10px;
+    color: var(--el-color-primary);
+}
+
+.cover .infinite-list .infinite-list-item+.list-item {
+    margin-top: 10px;
+}
+</style>
