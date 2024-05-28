d888892054697eae1e231411538c8bc888724687
Mark Yang <haker0x04@163.com>
2024-05-28 14:59:53 +0800

    修改主页head，去掉注册登录按钮，放在了欢迎页

---
+++ src/views/WelcomePage/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <link rel="stylesheet" href="src/assets/css/welcomePage.css">
+    <div class="head">
+        <div class="logo"><img src="/src/assets/icons/logo.png" alt=""></div>
+        <div class="buttons">
+            <div class="bt1">登录</div>
+            <div class="bt2">注册</div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
